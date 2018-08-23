4b14a5286cfe6e01bc09d245ddc252493a99f687
Untitled <john.d.reddock@gmail.com>
2018-08-22 20:47:00 -0400

    header component

---
+++ prod/albums/src/component/header.js
@@ -0,0 +1,16 @@
+// Import libraries for making a component
+
+import React from 'react';
+import { Text }from 'react-native':
+
+
+// Make a component
+
+const Header = () => {
+    return <Text>Albums</Text>
+};
+
+
+// Make the component availbable to other parts of the app
+
+
