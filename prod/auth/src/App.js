1b062a524415f2d3f93e78aa520711fbbee99d30
Untitled <john.d.reddock@gmail.com>
2018-08-29 16:56:04 -0400

    firebase client setup

--- prod/auth/src/App.js
+++ prod/auth/src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyClY9Q_Pd-oYlPSE_WmOVFNLkjDB1-DUNY",
+            authDomain: "authentication-2e79e.firebaseapp.com",
+            databaseURL: "https://authentication-2e79e.firebaseio.com",
+            projectId: "authentication-2e79e",
+            storageBucket: "authentication-2e79e.appspot.com",
+            messagingSenderId: "955523906005"
+          });
+    
+    }
+
     render(){
         return (
             <View>
