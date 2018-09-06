60d0065596b636d6f54bc104a50ec2eca8c04fc8
Untitled <john.d.reddock@gmail.com>
2018-09-06 19:00:35 -0400

    added a touchable for list item selection

--- prod/tech_stack/src/components/ListItem.js
+++ prod/tech_stack/src/components/ListItem.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
     render() {
+        const { titleStyle } = styles;
+        const { id, title } = this.props.library.item;
+
         return (
-            <CardSection>
-                <Text style={styles.titleStyle}>
-                {this.props.library.item.title}</Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+              onPress={() => this.props.selectLibrary(id)}
+            >
+              <View>
+                <CardSection>
+                    <Text style={styles.titleStyle}>
+                    {title}</Text>
+                </CardSection>
+              </View>
+            </TouchableWithoutFeedback>            
         );
     }
 }
