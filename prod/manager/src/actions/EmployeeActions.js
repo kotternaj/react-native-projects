9d25d6c04a28554ae4730e191d17fb8c3dfeaed5
Untitled <john.d.reddock@gmail.com>
2018-09-19 18:32:06 -0400

    save employees to firebase

--- prod/manager/src/actions/EmployeeActions.js
+++ prod/manager/src/actions/EmployeeActions.js
@@ -1,9 +1,18 @@
-import { EMPLOYEE_UPDATE 
+import firebase from 'firebase';
+import { EMPLOYEE_UPDATE
+
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
     return {
-        EMPLOYEE_UPDATE,
+        type: EMPLOYEE_UPDATE,
         payload: { prop, value }
     };
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+    const { currentUser } = firebase.auth();
+
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift });
 };
