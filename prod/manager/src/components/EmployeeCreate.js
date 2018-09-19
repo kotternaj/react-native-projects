7fba3fa18a57ba78e16c2bca686b593c2ff3a147
Untitled <john.d.reddock@gmail.com>
2018-09-19 16:06:37 -0400

    style override

--- prod/manager/src/components/EmployeeCreate.js
+++ prod/manager/src/components/EmployeeCreate.js
@@ -1,5 +1,5 @@
 import React , { Component } from 'react';
-import { Picker } from 'react-native';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
 import { Card,CardSection,Input, Button } from './common';
@@ -27,7 +27,8 @@ class EmployeeCreate extends Component {
                     />
                 </CardSection>
 
-                <CardSection>
+                <CardSection style={{ flexDirection: 'column' }}>
+                    <Text style={styles.pickerTextStyle}>Shift</Text>
                     <Picker
                       style={{ flex: 1}}
                       selectedValue={this.props.shift}
@@ -54,7 +55,16 @@ class EmployeeCreate extends Component {
     }
 }
 
+const styles = {
+    pickerTextStyle: {
+        fontSize: 18,
+        paddingLeft: 20
+    }
+}
+
 const mapStateToProps = (state) => {
     const { name, phone, shift} = state.employeeForm;
+    
+    return { name, phone , shift};
 }
 export default connect(mapStateToProps, { employeeUpdate }) (EmployeeCreate);
--- prod/manager/src/reducers/EmployeeFormReducer.js
+++ prod/manager/src/reducers/EmployeeFormReducer.js
@@ -1,11 +1,12 @@
 import {
     EMPLOYEE_UPDATE
+    
 } from '../actions/types';
 
 const INITIAL_STATE = {
-    name='',
-    phone='',
-    shift=''
+    name:'',
+    phone:'',
+    shift:''
 };
 
 export default (state = INITIAL_STATE, action ) => {
