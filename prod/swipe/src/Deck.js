f0dae131a42f15547d554b077c76665567597e07
Untitled <john.d.reddock@gmail.com>
2018-10-02 17:49:50 -0400

    render list of items

--- prod/swipe/src/Deck.js
+++ prod/swipe/src/Deck.js
@@ -1,10 +1,18 @@
-import React, { Componeent } from 'react';
+import React, { Component } from 'react';
 import  { View, Animated } from 'react-native';
 
 class Deck extends Component {
+    renderCards(){
+      return this.props.data.map(item => {
+          return this.props.renderCard(item)
+      });
+    }
+    
     render() {
         return (
-            <View />
+            <View>
+                {this.renderCards()}
+            </View>
         );
     }
 }
