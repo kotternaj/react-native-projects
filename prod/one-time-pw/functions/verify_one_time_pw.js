67acb2d181fba86dfb2f97a3693c368cb302e3e4
Untitled <john.d.reddock@gmail.com>
2018-10-22 20:54:30 -0400

    invalidation of user codes

--- prod/one-time-pw/functions/verify_one_time_pw.js
+++ prod/one-time-pw/functions/verify_one_time_pw.js
@@ -6,4 +6,19 @@ module.exports = function(req, res) => {
 
     const phone = String(req.body.phone).replace(/[^\d]/g, '');
     const code = parseInt(code);
+
+    admin.auth().getUser(phone)
+      .then(() => {
+          const ref = admin.database().ref('users/' + phone);
+          ref.on('value', snapshot => {
+              const user = snapshot.val();
+
+              if (user.code !==code || !user.codeValid) {
+                  return res.status(422).send({ error: 'Code not valid'});
+              }
+
+              ref.update({ codeValid: false });
+           });
+      })
+      .catch((err) => res.status(422).send({ error: err }))
 }
