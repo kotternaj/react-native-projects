6d582603632d492e8de37e14d0af92801abc3107
Untitled <john.d.reddock@gmail.com>
2018-10-22 20:45:20 -0400

    verifying one time pw

--- prod/one-time-pw/functions/request_one_time_pw.js
+++ prod/one-time-pw/functions/request_one_time_pw.js
@@ -9,5 +9,23 @@ module.exports = function(req, res) {
   const phone = String(req.body.phone).replace(/[^\d]/g, '');
 
   admin.auth().getUser(phone)
-    
+    .then(userRecord => {
+        const code = Math.floor((Math.random() * 8999 + 1000));
+
+        twilio.messages.create({
+        body: 'Your code is ' + code,
+        to: phone,
+        from: '+18053167032'
+        }, (err) => {
+        if (err) { return res.status(422).send(err); }
+
+        admin.database().ref('users/' + phone)
+            .update({ code: code, codeValid: true }, () => {
+            res.send({ success: true });
+            });
+        })
+    })
+    .catch((err) => {
+        res.status(422).send({ error: err });
+    });
 }
