db4fc239a5e0d0d45203ca8c8115ddb9c73bad0f
Untitled <john.d.reddock@gmail.com>
2018-10-23 22:00:08 -0400

    handle submit

--- prod/firebase-auth/components/SignUpForm.js
+++ prod/firebase-auth/components/SignUpForm.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
 
 class SignUpForm extends Component {
+    state = { phone: '' };
+
+    handleSubmit = async () => {
+        try {
+            await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone });
+            await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone });
+        } catch (err) {
+            console.log(err);
+        }
+        }
+
   render() {
       return (
+        <View>
           <View style={{ marginBottom: 10 }}>
               <FormLabel>Enter Phone Number</FormLabel>
-              <FormInput />
+              <FormInput 
+                value={this.state.phone}
+                onChangeText={phone => this.setState({ phone })}
+              />
               <Button title="Submit" />
           </View>
+          <Button onPress={this.handleSubmit} title="Submit"/>
+        </View>
       );
   }
 }
